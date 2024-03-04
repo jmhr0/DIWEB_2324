8e5e1791cbe40799e3d6add685de3ece33693686
Juan Manuel <jherram0603@g.educaand.es>
2024-03-04 14:59:46 +0100

    elementos interactivos en landing page

--- AccesibilidadForm/src/assets/scripts/main.js
+++ AccesibilidadForm/src/assets/scripts/main.js
@@ -1,2 +1,24 @@
-console.log("Hola Mundo")
+function onIniciar() {
+  console.log("Hola Mundo");
 
+
+  const botonesTab = document.querySelectorAll(".listaBotones__botonTab");
+  const bloquesTexto = document.querySelectorAll(".contenedorTexto__bloque");
+
+
+  botonesTab.forEach((boton, i) => {
+    console.log(boton)
+
+    boton.addEventListener("click", () => {
+      botonesTab.forEach((botonF, j) => {
+        botonF.className = botonF.className.replace(/\bactivo\b/g, '');
+        bloquesTexto[j].classList.remove("activo")
+      });
+      boton.className += " activo"; 
+      bloquesTexto[i].classList.add("activo")
+    });
+  });
+
+}
+
+window.onload = onIniciar;
---
+++ landingPage-JMHR/readme.md
@@ -0,0 +1,4 @@
+Indica qué derechos de propiedad tienen los elementos
+seleccionados, qué implican dichos derechos y también
+indica las fuentes desde donde se ha obtenido la
+información.
