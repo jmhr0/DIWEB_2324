65705a978285fd383a406ea09fd30fc9f684da9e
Juan Manuel <jherram0603@g.educaand.es>
2024-03-07 14:55:14 +0100

    codigo en clase

--- AccesibilidadForm/src/assets/scripts/main.js
+++ AccesibilidadForm/src/assets/scripts/main.js
@@ -1,6 +1,20 @@
 function onIniciar() {
   console.log("Hola Mundo");
 
+  var acc = document.getElementsByClassName("acordeon");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
 
   const botonesTab = document.querySelectorAll(".listaBotones__botonTab");
   const bloquesTexto = document.querySelectorAll(".contenedorTexto__bloque");
